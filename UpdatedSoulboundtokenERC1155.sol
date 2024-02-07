a516c07274d85e9954f84a6ebe7d1fe31164f996
HarshilMinddeft <157495886+HarshilMinddeft@users.noreply.github.com>
2024-02-07 12:10:19 +0530

    Add files via upload

---
+++ UpdatedSoulboundtokenERC1155.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+//This contract sbt tokens once minted can be teansfered once only to another contract
+
+contract Valorant is ERC1155, Ownable, ERC1155Burnable, ERC1155Supply {
+    uint256 public constant SOVA = 0;
+    uint256 public constant BRIMSTON = 1;
+
+    // Mapping to track whether a token has been transferred
+    mapping(uint256 => bool) private _transferred;
+
+    constructor(address initialOwner) ERC1155("ipfs://QmXT1U6Q11i1cPouzcKWbzAyZBb78nmxEtNQMx5ioh2wtf/{id}.json") Ownable(initialOwner) {
+        _mint(msg.sender, SOVA, 10, "");
+        _mint(msg.sender, BRIMSTON, 10, "");
+    }
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function uri(uint256 _tokenId) override public pure returns (string memory) {
+        return string(
+            abi.encodePacked(
+                "ipfs://QmXT1U6Q11i1cPouzcKWbzAyZBb78nmxEtNQMx5ioh2wtf/",
+                Strings.toString(_tokenId),
+                ".json"
+            )
+        );
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public onlyOwner {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    // Override safeTransferFrom to enforce one-time transfer after minting
+    function safeTransferFrom(address from, address to, uint256 id, uint256 value, bytes memory data) public override virtual {
+    //  address sender = _msgSender();
+        require(!_transferred[id], "This is a Soulbound token. It cannot be transferred after the first transfer.");
+        _safeTransferFrom(from, to, id, value, data);
+        _transferred[id] = true; // Mark the token as transferred
+    }
+
+      function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory values,
+        bytes memory data
+    ) public virtual override {
+        // address sender = _msgSender();
+        for (uint256 i = 0; i < ids.length; i++) {
+            require(!_transferred[ids[i]], "This tokens are Soulbound token. You cannot transfer them.");
+        }
+        // require(from == address(0) || to == address(0), "This is a Soulbound token. It can only be burned by the token owner.");
+        // if (from != sender && !isApprovedForAll(from, sender)) {
+        //     revert ERC1155MissingApprovalForAll(sender, from);
+        // }
+        _safeBatchTransferFrom(from, to, ids, values, data);
+        for (uint256 i = 0; i < ids.length; i++) {
+            _transferred[ids[i]] = true; // Mark each token in the batch as transferred
+        }
+        }
+
+    function _update(address from, address to, uint256[] memory ids, uint256[] memory values)
+        internal
+        override(ERC1155, ERC1155Supply)
+    {
+        super._update(from, to, ids, values);
+    }
+}
