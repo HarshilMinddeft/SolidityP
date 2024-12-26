f92431581bb53ef11bea1ca7be23cefc92ed991b
HarshilMinddeft <157495886+HarshilMinddeft@users.noreply.github.com>
2024-12-26 16:49:15 +0530

    Update SuperContractERC20.sol

--- SimpleSuperContracts/UpdatedSuperContracts/SuperContractERC20.sol
+++ SimpleSuperContracts/UpdatedSuperContracts/SuperContractERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./Erc20Super.sol";
+import "./SuperTokenWrapUnwrap.sol";
 
 contract StreamManager is ReentrancyGuard{
     
